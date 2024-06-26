cca2d9b9f660c7a24d3a3c8accd8f1b371a15da8
Matias Espina <matias@devbite.de>
2024-06-26 10:34:35 +0200

    mobile pdf view fixed

--- src/components/PdfViewer.tsx
+++ src/components/PdfViewer.tsx
@@ -6,10 +6,26 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 
+
+if (typeof Promise.withResolvers === 'undefined') {
+    if (window)
+        // @ts-expect-error This does not exist outside of polyfill which this is doing
+        window.Promise.withResolvers = function () {
+            let resolve, reject;
+            const promise = new Promise((res, rej) => {
+                resolve = res;
+                reject = rej;
+            });
+            return { promise, resolve, reject };
+        };
+}
+
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
+  'pdfjs-dist/legacy/build/pdf.worker.min.mjs',
   import.meta.url,
 ).toString();
+
+
 
 interface PdfViewerProps {
   fileUrl: string;
