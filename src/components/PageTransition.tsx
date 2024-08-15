8e08888977ecbbedcfb0b2afb081485d2e9e096e
Matias Espina <matias@devbite.de>
2024-08-15 17:32:29 +0200

    Transitions between pages extended 0,1s

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 // src/components/PageTransition.tsx
 import { motion } from 'framer-motion';
@@ -8,7 +8,7 @@ import { usePathname } from 'next/navigation';
 const variants = {
   hidden: { opacity: 0 },
   enter: { opacity: 1, transition: { duration: 0.0 } },
-  exit: { opacity: 0, transition: { duration: 0.1 } },
+  exit: { opacity: 0, transition: { duration: 0.2 } },
 };
 
 interface PageTransitionProps {
@@ -25,7 +25,7 @@ const PageTransition: React.FC<PageTransitionProps> = ({ children }) => {
       initial="hidden"
       animate="enter"
       exit="exit"
-      style={{ height: '100%'}}
+      style={{ height: '100%' }}
     >
       {children}
     </motion.div>
