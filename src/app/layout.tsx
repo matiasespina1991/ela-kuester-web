3429f3b103507c693cdbcbf9af311ffd4bd48505
Matias Espina <matias@devbite.de>
2024-08-15 18:24:32 +0200

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
 // src/app/layout.tsx
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
 import ClientLayout from './ClientLayout';
-import { FirstLoadProvider } from "@/context/FirstLoadContext";
+import { FirstLoadProvider } from '@/context/FirstLoadContext';
 
 export const metadata = {
-  title: "Ela Kuester",
-  description: "London based fashion designer, stylist and artist.",
+  title: 'Ela Kuester',
+  description: 'London based fashion designer, stylist and artist.',
+  openGraph: {
+    images: [
+      {
+        url: '/images/opengraph-image.jpg',
+        alt: 'Ela Kuester Logo',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html style={{ height: '100%' }} lang="en">
       <body style={{ height: '100%' }}>
-         
-        <ClientLayout>
-          {children}
-        </ClientLayout>
-
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
