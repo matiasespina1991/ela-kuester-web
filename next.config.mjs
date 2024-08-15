65ecd4bd56edf3b8ed3bcc3cd47d27bac09b6c15
Matias Espina <matias@devbite.de>
2024-08-15 20:04:26 +0200

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  distDir: 'out',
   webpack: (config) => {
     config.resolve.alias.canvas = false;
     return config;
